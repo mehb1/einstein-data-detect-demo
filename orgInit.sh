c6d59135ca6b7611d44b3acad42f1a8b4bb5b11b
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-10-29 16:58:11 -0700

    first cut

---
+++ orgInit.sh
@@ -0,0 +1,17 @@
+# Create the demo org
+sfdx shane:org:create -f config/project-scratch-def.json -d 5 -s --wait 60 --userprefix data -o detect.demo
+
+# Install Einstein Data Detect
+sfdx force:package:install -w 20 -r -p 04t5e000000zR3E
+
+# Push the metadata into the new scratch org.
+sfdx force:source:push
+
+# Set the default password.
+sfdx shane:user:password:set -g User -l User -p salesforce1
+
+# Open the org.
+sfdx force:org:open
+
+# Import the data required by the demo
+sfdx automig:load --inputdir ./data
