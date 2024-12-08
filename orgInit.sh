641a13b9a141b30730934a878528a2c0c1a1f09e
mehboob <77236545+zeb0x01@users.noreply.github.com>
2024-12-08 18:16:31 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e detect.demo
+sfdx login:functions:jwt --clientid 3MVG9CEn_O3jvv0zQGZ3RC8AeiKOvTHGF1AyKTPEhceKFjU7hrwn1JO3h.JnCWqOUo.ZQ9iGALbY1Fy_taX3y --username deploybot@dxhub.org --keyfile /app/tmp/server.key
 sf package install -w 20 -r -p 04t5e000000aVeP
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
